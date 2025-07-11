ee70dcf6dc1e6f13513efa1eae05f063dd70ab34
Elizabeth Wynn <preevete@gmail.com>
2025-07-11 00:40:28 -0600

    tag types

---
+++ src/domain/types/tag.ts
@@ -0,0 +1,12 @@
+import { Schema } from "effect"
+import { TagId, Slug } from "./branded"
+
+/**
+ * Tag - A simple label for organizing and querying entities
+ * Used for categorizing snippets and compositions
+ */
+export const Tag = Schema.Struct({
+  id: TagId,
+  name: Slug
+})
+export type Tag = typeof Tag.Type
